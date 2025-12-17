d298ddc7ec96b06da62a9c12c27d624c08668f54
Yash <yashuvarmora@gmail.com>
2025-12-17 16:09:55 +0530

    Login API And Mongodb

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient, Db } from 'mongodb';
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
+const uri = process.env.MONGODB_URI || 'mongodb+srv://renzmarr06_db_user:EbTynF0OhLWL1nbi@cycleiqcluster.5lgkwbk.mongodb.net/cycleiq';
 let client: MongoClient;
 let db: Db;
 
